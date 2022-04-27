ca84047e6a5c53105c18dd57f4cdd60840efd8a6
Ouyancheng <1024842937@qq.com>
2022-04-27 03:14:08 -0700

    handle wraparound

--- modules/common/src/delay.c
+++ modules/common/src/delay.c
@@ -1,12 +1,12 @@
 #include "delay.h"
 void delay_us(uint64_t us) {
     uint64_t current; 
-    uint64_t target; 
-    current = get_arch_counter(); 
-    target = current + us * 24UL; 
+    uint64_t initial; 
+    us *= 24UL; 
+    initial = get_arch_counter(); 
     do {
         current = get_arch_counter(); 
-    } while (current <= target); 
+    } while (current - initial <= us); // NOTE: this handles wrap around 
 }
 void delay_ms(uint64_t ms) {
     return delay_us(ms * 1000UL); 
