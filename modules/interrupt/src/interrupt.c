4999f55b17987935a3b32f501866c4e7c0b95283
Ouyancheng <1024842937@qq.com>
2022-05-01 13:58:33 -0700

    minor

--- modules/interrupt/src/interrupt.c
+++ modules/interrupt/src/interrupt.c
@@ -34,7 +34,7 @@ void exception_trampoline(void) {
 }
 /*
 Here's what clang generates 
-(better than gcc, gcc also saves the vector registers (because it's turned on), but in a strange way): 
+(gcc also saves the vector registers (because it's turned on), but in a strange way): 
 0000000040001d88 <exception_trampoline>:
 40001d88: 13 01 01 e8  	addi	sp, sp, -384
 40001d8c: 23 3c 11 16  	sd	ra, 376(sp)
