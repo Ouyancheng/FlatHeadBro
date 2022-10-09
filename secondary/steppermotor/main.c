a8dc992636641fe5821ea12c978d9f34892f3c34
Ouyancheng <1024842937@qq.com>
2022-10-09 00:33:51 -0700

    add another notation

--- secondary/steppermotor/main.c
+++ secondary/steppermotor/main.c
@@ -4,15 +4,20 @@
 enum note_idx {
     C  = 0,
     Cs = 1,
+    Db = 1,
     D  = 2,
     Ds = 3, 
+    Eb = 3,
     E  = 4, 
     F  = 5, 
     Fs = 6,
+    Gb = 6,
     G  = 7, 
     Gs = 8, 
+    Ab = 8,
     A  = 9,
     As = 10,
+    Bb = 10,
     B  = 11,
 };
 
